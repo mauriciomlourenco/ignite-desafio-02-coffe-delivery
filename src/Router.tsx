5f09bdeddf1d806171e50643e8f9aced9be9eaf1
Mauricio Machado Lourenço <mauricio.mchd@gmail.com>
2023-02-22 21:19:34 -0300

    Header Component, Routes and Default Layout

---
+++ src/Router.tsx
@@ -0,0 +1,16 @@
+import { Route } from "react-router";
+import { Routes } from "react-router-dom";
+import { Defaultlayout } from "./layouts/Defaultlayout";
+import { CompleteOrderPage } from "./pages/CompleteOrder";
+import { HomePage } from "./pages/Home";
+
+export function Router() {
+    return (
+        <Routes>
+            <Route path="/" element={<Defaultlayout />}>
+                <Route path="/" element={<HomePage />}/>
+                <Route path="/completeOrder" element={<CompleteOrderPage />}/>
+            </Route>
+        </Routes>
+    )
+}
---
+++ src/styles/global.ts
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        outline: none;
+    }
+
+    body {
+        background: ${({ theme }) => theme.colors["base-background"]};
+        color: ${({ theme }) => theme.colors["base-text"]};
+        -webkit-font-smoothing: antialiased;
+    }
+
+    body, input, textarea, button {
+        font-family: 'Roboto', sans-serif;
+        font-weight: 400;
+        font-size: 1rem;
+    }
+
+    button {
+        cursor: pointer;
+    }
+
+    a {
+        text-decoration: none;
+    }
+`;
