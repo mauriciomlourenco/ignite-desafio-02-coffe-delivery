34cb427914ee0c9bfe2fe164f84236151b221bd0
Mauricio Machado Lourenço <mauricio.mchd@gmail.com>
2023-02-28 17:38:07 -0300

    Finished Order Confirmed page

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,6 +3,7 @@ import { Routes } from 'react-router-dom'
 import { Defaultlayout } from './layouts/Defaultlayout'
 import { CompleteOrderPage } from './pages/CompleteOrder'
 import { HomePage } from './pages/Home'
+import { OrderConfirmedPage } from './pages/OrderConfirmed'
 
 export function Router() {
   return (
@@ -10,6 +11,7 @@ export function Router() {
       <Route path="/" element={<Defaultlayout />}>
         <Route path="/" element={<HomePage />} />
         <Route path="/completeOrder" element={<CompleteOrderPage />} />
+        <Route path="/orderConfirmed" element={<OrderConfirmedPage />} />
       </Route>
     </Routes>
   )
